4aae26587346f3b90217e94981bcbc6a1fbc30a8
yamanaka776 <92915718+yamanaka776@users.noreply.github.com>
2023-11-17 19:20:00 +0900

    aa

---
+++ blog/astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), sitemap(), partytown()]
+});
