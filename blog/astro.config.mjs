03b4845c26ec659547c86e62af5f03234a91a3df
yamanaka776 <92915718+yamanaka776@users.noreply.github.com>
2023-11-17 22:51:00 +0900

    ブログコンポーネント

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import partytown from "@astrojs/partytown";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), partytown()]
+  integrations: [tailwind(), sitemap(), partytown(), mdx()]
 });
